0f0f1adb509be645ec12d25761c31b1e2194b9ce
Jeremie Olivier <jeremie.olivier.pro@gmail.com>
2021-03-07 22:31:40 +0100

    fix typo in warning disclaimer and fix styling

--- inject-warning-header.js
+++ inject-warning-header.js
@@ -3,9 +3,20 @@ let body = document.querySelector('body');
 
 let warningContainer = document.createElement('div');
 warningContainer.innerHTML = `  
-<div style="background-color: red;color: white;text-align: center;">
-    <p>This is an <b>ununfocial</b>, community driven proposal translation for the website https://eco.com. Translation here can be missleading or incorect. </p>
-    <p>For <b>offical</b> information, please <b>go to <a href="https://eco.com">https://eco.com</a> </b> </p>
+<div style="
+            background-color: red;
+            color: white;
+            text-align: center;
+            font-size: 16px;
+            padding: 25px;
+            position: fixed;
+            width: 100%;
+            z-index: 50000;
+">
+    <p>This is an <b>unofficial</b>, community-driven proposal translation for the website https://eco.com. Translation here can be misleading or incorrect.<br>
+        If you find a missing or incorrect translation and want to contribute go check : https://github.com/eco-translations/eco-translations.github.io
+    </p>
+    <p>For <b>official</b> information, please <b>go to <a href="https://eco.com">https://eco.com</a> </b> </p>
 </div>
 `;
 body.insertBefore(warningContainer, body.firstChild);
